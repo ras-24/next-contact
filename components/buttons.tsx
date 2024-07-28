38157bc2239a850ac5dddc826fc4b7cf9577c516
Ras <siregar.riyando@gmail.com>
2024-07-29 04:55:58 +0800

    Adding Create Data & Form Validation

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import { IoAddSharp, IoPencil, IoTrashOutline } from "react-icons/io5";
+import { useFormStatus } from "react-dom";
+import clsx from "clsx";
 
 export const CreateButton = () => {
   return (
@@ -28,6 +32,30 @@ export const DeleteButton = () => {
   return (
     <button className="rounded-sm border p-1 hover:bg-gray-100">
       <IoTrashOutline size={20} />
+    </button>
+  );
+};
+
+export const SubmitButton = ({ label }: { label: string }) => {
+  const { pending } = useFormStatus();
+
+  const className = clsx(
+    "text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-sm text-sm w-full px-5 py-3 text-center", {
+      "opacity-50 cursor-progress": pending
+    }
+  );
+
+  return (
+    <button
+      type="submit"
+      className={className}
+      disabled={pending}
+    >
+      {label === "save" ? (
+        <span>{pending ? "Saving..." : "Save"}</span>
+      ) : (
+        <span>{pending ? "Updating..." : "Update"}</span>
+      )}
     </button>
   );
 };
